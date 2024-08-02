c217d0f0addf813e0dc186513feeaf19362b9326
Sam Kachergius <skachergius@gmail.com>
2024-08-01 19:22:58 -0500

    added starter code to home and add recipe pages

---
+++ client/src/pages/AddRecipe.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import RecipeForm from '../components/RecipeForm';
+
+const AddRecipe = () => {
+    return (
+        <div>
+            <h1>Add a new recipe</h1>
+            <RecipeForm />
+        </div>
+    );
+};
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import RecipeList from '../components/RecipeCard'
+
+const home = () => {
+    return (
+        <div>
+            <h1>Recipe Sharing App</h1>
+        </div>
+    );
+};
+
+export default Home;
+
